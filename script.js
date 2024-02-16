ae5fe9e129736406ac518ee317d305777b7bd13f
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2024-02-16 11:50:53 +0530

    Add functionality to ask user for book input and storing them in array

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 let addBook = document.getElementById("add");
+let bookName;
+
 addBook.addEventListener("click", function () {
-  console.log("The add book button was clicked!");
+  bookName = prompt("Enter a book: ");
+  addBookToLibrary();
+});
+
+//Array to store the books
+const myLibrary = [];
+
+//Constructor function to create "book" objects
+function Book() {}
+
+//Function to store the "book" objects into the array
+function addBookToLibrary() {
+  myLibrary.push(bookName);
+  displayBook(bookName);
+}
+
+//Function to display books
+function displayBook(bookName) {
+  console.log(bookName);
+}
+
+let showBook = document.getElementById("show");
+showBook.addEventListener("click", function () {
+  console.log(myLibrary);
 });
