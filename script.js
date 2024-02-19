2fe769aa603b885b72853c83ef6ccab815900bd8
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2024-02-19 18:55:16 +0530

    Add comments for explanation and organised code

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-let addBook = document.getElementById("add");
-let bookName;
-let removeButton;
-var readStatus;
-
-// addBook.addEventListener("click", function () {
-//   bookName = prompt("Enter a book: ");
-//   addBookToLibrary();
-// });
-
 //Array to store the books
 const myLibrary = [];
 
-//Constructor function to create "book" objects
+// Selecting the button to add books
+let addBook = document.getElementById("add");
+//Showing the dialog box on the button click
+addBook.addEventListener("click", function () {
+  dialog.showModal();
+});
+
+// Selecting the dialog box that appears on clicking the button to add books
+const dialog = document.querySelector("dialog");
+
+// Selecting the form inside the dialog box
+const form = document.getElementById("book-form");
+// Function for when the form is submitted to add a book
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Storing the values entered in the fields in variables
+  const bookTitle = document.getElementById("book-title").value;
+  const authorName = document.getElementById("author-name").value;
+  const pageNumber = document.getElementById("page-number").value;
+  const readStatus = document.querySelector(
+    'input[name = "read-status"]:checked'
+  ).value;
+
+  //Resetting the form and closing the dialog box when the form is submitted
+  form.reset();
+  dialog.close();
+
+  // Making a book card by calling the constructor function
+  const bookName = new Book(bookTitle, authorName, pageNumber, readStatus);
+
+  //Storing the created book card into the array
+  myLibrary.push(bookName);
+
+  //When the form is submitted, the displayBook function is called to display the book card.
+  displayBook(bookName);
+});
+
+//Selecting the close button 'X' on the dialog box
+const closeButton = document.getElementById("close");
+//Function to close the dialog box using the close 'X' button
+closeButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  dialog.close();
+  form.reset();
+});
+
+//Constructor function to create the "Book" objects
 function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
@@ -28,32 +65,39 @@ function Book(title, author, pages, readStatus) {
       this.readStatus
     );
   };
-  // displayBook(bookName);
 }
 
 //Function to display books
 function displayBook(bookName) {
+  // Selecting the body area to place book cards
   const body = document.body;
   const libraryBody = document.getElementById("library-body");
+
+  // Making a book card to store details of one book
   const bookContainer = document.createElement("div");
+
+  // Book title
   const title = document.createElement("h3");
   title.innerHTML = bookName.title;
+  bookContainer.append(title);
+
+  // Book author
   const author = document.createElement("p");
   author.textContent = "Author: ";
+  author.append(bookName.author);
+  bookContainer.append(author);
+
+  // Book pages
   const pages = document.createElement("p");
   pages.textContent = "Pages: ";
+  pages.append(bookName.pages);
+  bookContainer.append(pages);
 
-  removeButton = document.createElement("button");
-
-  removeButton.innerHTML = "Remove book";
-  removeButton.classList.add("remove-book");
-
-  //Buton to print the index value of the book object
-  const indexButton = document.createElement("button");
-  indexButton.innerHTML = "Check index";
-
-  // Button to change the status
+  // Selecting the button to change the Read or Not read status
   const changeStatus = document.createElement("button");
+  bookContainer.append(changeStatus);
+
+  // Adding "Read" or "Not read" styling class to button depending on the option selected in the dialog form
   if (bookName.readStatus === "Read") {
     changeStatus.classList.add("read-book");
   } else if (bookName.readStatus === "Not read") {
@@ -61,101 +105,46 @@ function displayBook(bookName) {
   }
   changeStatus.innerHTML = bookName.readStatus;
 
+  // Calling the toggleStatus() function from the Book prototype to change the readStatus property on individual Book objects
   changeStatus.addEventListener("click", function () {
     bookName.toggleStatus(changeStatus);
   });
-  author.append(bookName.author);
-  pages.append(bookName.pages);
 
-  bookContainer.append(title);
-  bookContainer.append(author);
-  bookContainer.append(pages);
-  bookContainer.append(status);
+  // Making the remove button to remove books from the library
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = "Remove book";
+  removeButton.classList.add("remove-book");
   bookContainer.append(removeButton);
-  bookContainer.append(indexButton);
-  bookContainer.append(changeStatus);
-  bookContainer.classList.add("book-container");
-  libraryBody.append(bookContainer);
-  body.append(libraryBody);
-
-  // calling the function to change the read status
-
+  // Function for the remove button to remove books from the array
   removeButton.addEventListener("click", function () {
     myLibrary.splice(myLibrary.indexOf(bookName), 1);
     libraryBody.removeChild(bookContainer);
   });
 
+  //Buton to print the index value of the book object
+  const indexButton = document.createElement("button");
+  indexButton.innerHTML = "Check index";
+  bookContainer.append(indexButton);
+  // Function to check the index number of each individual book in the array
   indexButton.addEventListener("click", function () {
     console.log(myLibrary.indexOf(bookName));
   });
 
-  // dynamicToggleButton.checked = bookData.toggleButtonState;
+  // Adding class for styling of each book card
+  bookContainer.classList.add("book-container");
+
+  // Appending dynamically created book cards and library body to the main body
+  libraryBody.append(bookContainer);
+  body.append(libraryBody);
 }
 
-//adding function to the removeButton button:
-
+// Function for the Show Array button
 let showBook = document.getElementById("show");
 showBook.addEventListener("click", function () {
   console.log(myLibrary);
 });
 
-//Dialog Box functionality
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog" + "button");
-// const closeButton = document.querySelector("dialog button");
-
-//Showing the dialog box on the button click
-addBook.addEventListener("click", function () {
-  dialog.showModal();
-});
-
-// //Closing the dialog box by clicking on the "Close" button
-// closeButton.addEventListener("click", function () {
-//   dialog.close();
-// });
-
-const form = document.getElementById("book-form");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const bookTitle = document.getElementById("book-title").value;
-  const authorName = document.getElementById("author-name").value;
-  const pageNumber = document.getElementById("page-number").value;
-  const readStatus = document.querySelector(
-    'input[name = "read-status"]:checked'
-  ).value;
-
-  form.reset();
-  dialog.close();
-
-  bookName = new Book(bookTitle, authorName, pageNumber, readStatus);
-  console.log(bookName);
-  myLibrary.push(bookName);
-
-  //When the form is submitted, the displayBook function is called to display the book card.
-  displayBook(bookName);
-});
-
-//Function to store the "book" objects into the array
-// function addBookToLibrary() {
-//   myLibrary.push(bookName);
-// }
-
-//Function to close the dialog box using the close 'X' button
-const closeButton = document.getElementById("close");
-closeButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  dialog.close();
-  form.reset();
-});
-
-// Selecting the toggle switch using JavaScript
-
-// toggleSwitch.addEventListener("click", function () {
-
-// });
-
-//Function to toggle a book's read status
+// toggleStatus() function on the Book prototype that changes the read status of individual books
 Book.prototype.toggleStatus = function (buttonValue) {
   if (buttonValue.innerHTML === "Read") {
     buttonValue.innerHTML = "Not read";
